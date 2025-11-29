5c3cf41d9a0152e9c65720091861f9354e3538be
lrs <lrs>
2025-11-29 10:58:09 +0000

    1.1.2

--- hw870/hw870.ino
+++ hw870/hw870.ino
@@ -18,6 +18,49 @@ void setup(){
   Serial.println("Sensor 1 - A0: pin 2, D0: pin 16");
   Serial.println("Sensor 2 - A0: pin 22, D0: pin 21");
   Serial.println();
+          pinMode(M1, OUTPUT);
+  pinMode(M2, OUTPUT);
+  pinMode(M3, OUTPUT);
+  pinMode(M4, OUTPUT);
+  // additional direct-write motor pins (match code.ino)
+  pinMode(P17, OUTPUT);
+  pinMode(P19, OUTPUT);
+  pinMode(P18, OUTPUT);
+  pinMode(P23, OUTPUT);
+  pinMode(P32, OUTPUT);
+  pinMode(P33, OUTPUT);
+  pinMode(P25, OUTPUT);
+  pinMode(P26, OUTPUT);
+  // user button on GPIO0 (pulled up)
+  pinMode(0, INPUT_PULLUP);
+  // ensure motors off
+  digitalWrite(M1, LOW);
+  digitalWrite(M2, LOW);
+  digitalWrite(M3, LOW);
+  digitalWrite(M4, LOW);
+  // ensure additional pins off
+  digitalWrite(P17, LOW);
+  digitalWrite(P19, LOW);
+  digitalWrite(P18, LOW);
+  digitalWrite(P23, LOW);
+  digitalWrite(P32, LOW);
+  digitalWrite(P33, LOW);
+  digitalWrite(P25, LOW);
+  digitalWrite(P26, LOW);
+  const int M1 = 27;
+const int M2 = 14;
+const int M3 = 12;
+const int M4 = 13;
+
+// Additional GPIOs used by simple `code.ino` pattern
+const int P17 = 17;
+const int P19 = 19;
+const int P18 = 18;
+const int P23 = 23;
+const int P32 = 32;
+const int P33 = 33;
+const int P25 = 25;
+const int P26 = 26;//
 }
 
 void loop(){
