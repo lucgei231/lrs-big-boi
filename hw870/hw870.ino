17911c389e32cc101f2bad8e065b6edcf9566499
lrs <lrs>
2026-01-17 13:26:38 +0000

    1.4.3

--- hw870/hw870.ino
+++ hw870/hw870.ino
@@ -143,12 +143,12 @@ void loop(){
     digitalWrite(M4_PIN1, HIGH); digitalWrite(M4_PIN2, LOW);
     digitalWrite(M5_PIN1, HIGH); digitalWrite(M5_PIN2, LOW);
     digitalWrite(M6_PIN1, HIGH); digitalWrite(M6_PIN2, LOW);
-    delay(1000);
+    delay(1500);
         digitalWrite(M1_PIN1, LOW); digitalWrite(M1_PIN2, HIGH);
     digitalWrite(M2_PIN1, LOW); digitalWrite(M2_PIN2, HIGH);
     digitalWrite(M3_PIN1, LOW); digitalWrite(M3_PIN2, HIGH);
     digitalWrite(M4_PIN1, LOW); digitalWrite(M4_PIN2, HIGH);
     digitalWrite(M5_PIN1, LOW); digitalWrite(M5_PIN2, HIGH);
     digitalWrite(M6_PIN1, LOW); digitalWrite(M6_PIN2, HIGH);
-    delay(300);
+    delay(1500);
 }
